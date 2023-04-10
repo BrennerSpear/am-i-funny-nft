2df854750805d09d0647dc2804b67d99528bb217
Brenner Spear <blspear@gmail.com>
2023-04-10 17:54:41 -0400

    mainnet flag

--- script/deploy.s.sol
+++ script/deploy.s.sol
@@ -8,6 +8,24 @@ contract DeployGenericPfp is Script {
     function run() external {
         vm.startBroadcast();
 
+        string memory name = "Vanderbilt Commodores";
+        string memory symbol = "VANDY";
+        string memory slug = "commodore";
+
+        // string memory name = "Llama Pfp";
+        // string memory symbol = "LMAPFP";
+        // string memory slug = "llama-pfp";
+
+        // string memory name = "Robo Nova";
+        // string memory symbol = "RBNVA";
+        // string memory slug = "robo-nova";
+
+        // string memory name = "CDMX Axolotls";
+        // string memory symbol = "AXTL";
+        // string memory slug = "cdmx-axolotls";
+
+
+
         bool isMainnet = block.chainid == 1;
 
         // use a different address if the chain is mainnet
@@ -16,13 +34,13 @@ contract DeployGenericPfp is Script {
         uint256 mintsPerAddress = isMainnet ? 1 : 1000;
         bool isMintActive = isMainnet ? false : true;
         address metagameAddress = 0x9D8395A406FA264DeA71671c772269e844264E8C; // TODO Update this address
-        string memory metadataFolderUri = "https://dev.avatar-studio.xyz/api/metadata/";
-        string memory contractMetadataUrl = "https://dev.avatar-studio.xyz/api/contract-metadata/";
+        string memory metadataFolderUri = isMainnet ? "https://avatar-studio.xyz/api/metadata/" : "https://dev.avatar-studio.xyz/api/metadata/";
+        string memory contractMetadataUrl = isMainnet ? "https://avatar-studio.xyz/api/contract-metadata/" : "https://dev.avatar-studio.xyz/api/contract-metadata/";
 
          genericPfp genericPfpInstance = new genericPfp(
-            "Robo Nova", // name
-            "RBNVA", // symbol
-            "robo-nova", // slug
+            name, // name
+            symbol, // symbol
+            slug, // slug
             metadataFolderUri, // metadata folder uri
             mintsPerAddress, // mints per address
             contractMetadataUrl, // opensea contract metadata url
